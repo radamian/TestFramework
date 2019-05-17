0a210167304e561053696dc64f8d1518d25d7293
Raluca Damian <raluca.damian@endava.com>
2019-05-17 16:23:20 +0300

    My first commit to a new repo for automation framework +  tests draft

---
+++ Tests/src/test/java/com/pluralsight/UnitTest.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class UnitTest {
+    @Test
+    public void canGoToHomePage() {
+/*        Pages.homePage().goTo();
+        assertTrue(Pages.homePage().isAt());*/
+
+
+    }
+
+
+}
